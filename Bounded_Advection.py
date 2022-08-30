8bf83ab2d3e6e9171a3dad2aed10fab9a5fd8596
molin.han <molin.han20@imperial.ac.uk>
2022-08-30 23:58:38 +0100

    working on BA

--- Bounded_Advection.py
+++ Bounded_Advection.py
@@ -101,7 +101,11 @@ assemble(Courant_num_form_minus , tensor=Courant_num_minus )
 Courant_minus.assign(Courant_num_minus /Courant_denom_minus )
 
 
-#Set for the second limiter.
+#Set for the flux limiter for density.
+
+rho1 = Function(V); rho2 = Function(V)
+
+
 beta = Function(DG1)
 beta1 = Function(DG1)
 beta2 = Function(DG1)
@@ -116,6 +120,38 @@ rho2_bar = Function(DG1)
 rho2_hat_bar = Function(DG1)
 
 
+#set for the expression for beta
+c_plus = Courant_plus * rho / rho_hat_bar
+c_minus = Courant_minus * rho / rho_hat_bar
+
+c1_plus = Courant_plus * rho1 / rho1_hat_bar
+c1_minus = Courant_minus * rho1 / rho1_hat_bar
+
+c2_plus = Courant_plus * rho2 / rho2_hat_bar
+c2_minus = Courant_minus * rho2 / rho2_hat_bar
+
+beta_expr = Max(0, Min(1, (1 + Courant_minus - Courant_plus)/(c_minus - c_plus - Courant_minus + Courant_plus)))
+
+beta1_expr = Max(0, Min(1, (1 + Courant_minus - Courant_plus)/(c1_minus - c1_plus - Courant_minus + Courant_plus)))
+
+beta2_expr = Max(0, Min(1, (1 + Courant_minus - Courant_plus)/(c2_minus - c2_plus - Courant_minus + Courant_plus)))
+
+
+
+
+#set the flux limiter for tracer q.
+alpha = Function(DG1)
+alpha1 = Function(DG1)
+alpha2 = Function(DG1)
+
+q_bar = Function(DG1)
+q_hat_bar = Function(DG1)
+
+q1_bar = Function(DG1)
+q1_hat_bar = Function(DG1)
+
+q1_bar = Function(DG1)
+q1_hat_bar = Function(DG1)
 
 
 
@@ -127,7 +163,7 @@ L1_rho = dtc*(rho*dot(grad(phi),u)*dx
           - (phi('+') - phi('-'))*(un('+')*rho('+') - un('-')*rho('-'))*dS)
 
 
-rho1 = Function(V); rho2 = Function(V)
+
 L2_rho = replace(L1_rho, {rho: rho1}); L3_rho = replace(L1_rho, {rho: rho2})
 
 drho = Function(V)
@@ -162,12 +198,13 @@ Fsproblem = LinearVariationalProblem(aFs, LFs, Fs)
 Fssolver = LinearVariationalSolver(Fsproblem)
 Fssolver.solve()
 Fsf,Fsi = split(Fs)
-Fn  = 0.5*(dot((Fsf+Fsi), n) + abs(dot((Fsf+Fsi), n)))
+Fnew = Fsf + Fsi
+Fn  = 0.5*(dot((Fnew), n) + abs(dot((Fnew), n)))
 
 #variational problem for q
-L1_q = dtc*(q*dot(grad(phi),Fs)*dx
-          - conditional(dot(Fs, n) < 0, phi*dot(Fs, n)*q_in, 0.0)*ds
-          - conditional(dot(Fs, n) > 0, phi*dot(Fs, n)*q, 0.0)*ds
+L1_q = dtc*(q*dot(grad(phi),Fnew)*dx
+          - conditional(dot(Fnew, n) < 0, phi*dot(Fnew, n)*q_in, 0.0)*ds
+          - conditional(dot(Fnew, n) > 0, phi*dot(Fnew, n)*q, 0.0)*ds
           - (phi('+') - phi('-'))*(Fn('+')*q('+') - Fn('-')*q('-'))*dS)
 
 q1 = Function(V); q2 = Function(V)
@@ -210,73 +247,138 @@ if step % output_freq == 0:
 
 
 
-#Apply the limiter to q and density first and find beta.
+#Apply the limiter to q and density first and find beta, alpha.
 rho_bar.project(rho)
 limiter.apply(rho)
 rho_hat_bar.project(rho)
-beta.assign(max(0, min(1, (1 + Courant_minus - Courant_plus*rho_hat_bar/rho_bar)
-/(Courant_plus - Courant_plus*rho_hat_bar/rho_bar))))
+#Here rho is the value after applying the Kuzmin limiter i.e. rho_hat
+beta.assign(beta_expr)
 #apply the limiting scheme
 rho.project(rho_hat_bar + beta * (rho - rho_hat_bar))
 print(rho.dat.data.max())
-
+print(rho.dat.data.min())
 
 
 #Main body
 
 while t < T - 0.5*dt:
-    #solve the density
+    #solve the density and the bounded advection
+
+
     #first stage
+    
+    #For rho_1
     solv1_rho.solve()
     rho1.assign(rho + drho)
+
+    #Courant number should be recalculated
+    #c+
+    assemble(One*v*dx, tensor=Courant_denom_plus)
+    assemble(Courant_num_form_plus, tensor=Courant_num_plus)
+    Courant_plus.assign(Courant_num_plus/Courant_denom_plus)
+    #c-
+    assemble(One*v*dx, tensor=Courant_denom_minus )
+    assemble(Courant_num_form_minus , tensor=Courant_num_minus )
+    Courant_minus.assign(Courant_num_minus /Courant_denom_minus )
+
+    #rho limiting scheme, beta1 found.
     rho1_bar.project(rho1)
     limiter.apply(rho1)
     rho1_hat_bar.project(rho1)
-    beta1.assign(max(0, min(1, (1 + Courant_minus - Courant_plus*rho1_hat_bar/rho1_bar)
-    /(Courant_plus - Courant_plus*rho1_hat_bar/rho1_bar))))
+    beta1.assign(beta1_expr)
     #apply the limiting scheme
     rho1.project(rho1_hat_bar + beta1 * (rho1 - rho1_hat_bar))
+
+    #For Flux_1
+
+
+
+
+    #For q_1
+
+
+
+
+
+
+
 
     #second stage
+    #For rho_2
     solv2_rho.solve()
     rho1.assign(rho1+drho)
+    #Courant number should be recalculated
+    #c+
+    assemble(One*v*dx, tensor=Courant_denom_plus)
+    assemble(Courant_num_form_plus, tensor=Courant_num_plus)
+    Courant_plus.assign(Courant_num_plus/Courant_denom_plus)
+    #c-
+    assemble(One*v*dx, tensor=Courant_denom_minus )
+    assemble(Courant_num_form_minus , tensor=Courant_num_minus )
+    Courant_minus.assign(Courant_num_minus /Courant_denom_minus )
+
+    #limiter apply to rho1 another time.
     limiter.apply(rho1)
     rho1_hat_bar.project(rho1)
-    beta1.assign(max(0, min(1, (1 + Courant_minus - Courant_plus*rho1_hat_bar/rho1_bar)
-    /(Courant_plus - Courant_plus*rho1_hat_bar/rho1_bar))))
+    beta1.assign(beta1_expr)
     #apply the limiting scheme
     rho1.project(rho1_hat_bar + beta1 * (rho1 - rho1_hat_bar))
 
+    #Calculate for the second stage rho value.
     rho2.assign(0.75*rho + 0.25*(rho1))
     rho2_bar.project(rho2)
     limiter.apply(rho2)
     rho2_hat_bar.project(rho2)
-    beta2.assign(max(0, min(1, (1 + Courant_minus - Courant_plus*rho2_hat_bar/rho2_bar)
-    /(Courant_plus - Courant_plus * rho2_hat_bar/rho2_bar))))
+    beta2.assign(beta2_expr)
     #apply the limiting scheme
     rho2.project(rho2_hat_bar + beta2 * (rho2 - rho2_hat_bar))
+
+    #For Flux_2
+
+
+
+
+    #For q_2
+
+
+
+
+
+
 
     #third stage
+    #For rho
     solv3_rho.solve()
     rho2.assign(rho2+drho)
+    #Courant number should be recalculated
+    #c+
+    assemble(One*v*dx, tensor=Courant_denom_plus)
+    assemble(Courant_num_form_plus, tensor=Courant_num_plus)
+    Courant_plus.assign(Courant_num_plus/Courant_denom_plus)
+    #c-
+    assemble(One*v*dx, tensor=Courant_denom_minus )
+    assemble(Courant_num_form_minus , tensor=Courant_num_minus )
+    Courant_minus.assign(Courant_num_minus /Courant_denom_minus )
+
+    #limiter apply to rho2 another time.
     limiter.apply(rho2)
     rho2_hat_bar.project(rho2)
-    beta2.assign(max(0, min(1, (1 + Courant_minus - Courant_plus*rho2_hat_bar/rho2_bar)
-    /(Courant_plus - Courant_plus*rho2_hat_bar/rho2_bar))))
+    beta2.assign(beta2_expr)
     #apply the limiting scheme
     rho2.project(rho2_hat_bar + beta2 * (rho2 - rho2_hat_bar))
 
+    #Calculate for the rho value.
     rho.assign((1.0/3.0)*rho + (2.0/3.0)*(rho2))
     rho_bar.project(rho)
     limiter.apply(rho)
     rho_hat_bar.project(rho)
-    beta.assign(max(0, min(1, (1 + Courant_minus - Courant_plus*rho_hat_bar/rho_bar)
-    /(Courant_plus - Courant_plus*rho_hat_bar/rho_bar))))
+    beta.assign(beta_expr)
     #apply the limiting scheme
     rho.project(rho_hat_bar + beta * (rho - rho_hat_bar))
 
 
-    print(rho.dat.data.max())
+    print("rho_max=" rho.dat.data.max())
+    print("rho_min=" rho.dat.data.min())
 
     #solve the flux problem(each stage)
     Fssolver.solve()
@@ -314,11 +416,11 @@ while t < T - 0.5*dt:
 
 L2_err_rho = sqrt(assemble((rho - rho_init)*(rho - rho_init)*dx))
 L2_init_rho = sqrt(assemble(rho_init*rho_init*dx))
-print(L2_err_rho/L2_init_rho)
+print("error_rho =" L2_err_rho/L2_init_rho)
 
 L2_err_q = sqrt(assemble((q - q_init)*(q - q_init)*dx))
 L2_init_q = sqrt(assemble(q_init*q_init*dx))
-print(L2_err_q/L2_init_q)
+print("error_q ="L2_err_q/L2_init_q)
 
 
 nsp = 16
@@ -336,7 +438,7 @@ interval = 1e3 * output_freq * dt
 animation_rho = FuncAnimation(fig, animate, frames=rhos, interval=interval)
 animation_q = FuncAnimation(fig, animate, frames=qs, interval=interval)
 try:
-    animation_rho.save("DG_rho_1.mp4", writer="ffmpeg")
-    animation_q.save("DG_q_1.mp4", writer="ffmpeg")
+    animation_rho.save("BA_rho_1.mp4", writer="ffmpeg")
+    animation_q.save("BA_q_1.mp4", writer="ffmpeg")
 except:
     print("Failed to write movie! Try installing `ffmpeg`.")
