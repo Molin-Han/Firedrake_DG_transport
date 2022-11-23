4ca3e8879364332dc7a89a9136f8e66f571d0668
Molin Han <molin.han20@imperial.ac.uk>
2022-11-23 01:32:09 +0000

    nudging

--- BA_term1_Euler.py
+++ BA_term1_Euler.py
@@ -66,6 +66,8 @@ q_init = Function(V).assign(q)
 #solution list
 rhos = []
 qs = []
+rho_data = File('BA_Euler_rho.pvd')
+q_data = File('BA_Euler_q.pvd')
 
 
 
@@ -92,6 +94,16 @@ b = phi*rho_new*dq_trial*dx
 #elements
 n = FacetNormal(mesh)
 un = 0.5*(dot(u, n) + abs(dot(u, n)))
+
+
+
+
+#Set Kuzmin limiter
+limiter_rho = VertexBasedLimiter(V)
+limiter_q = VertexBasedLimiter(V)
+
+
+
 
 #Courant number setting
 def both(vec):
@@ -179,8 +191,150 @@ LFs = (
     + inner(wI,u)*rho*dx
     )
 Fs = Function(W)
+params_flux = {'ksp_type': 'preonly', 'pc_type': 'lu','mat_type': 'aij','pc_factor_mat_solver_type':'mumps'}
 Fsproblem = LinearVariationalProblem(aFs, LFs, Fs)
 Fssolver = LinearVariationalSolver(Fsproblem,solver_parameters=params_flux)
 Fssolver.solve()
 Fsf,phi_flux= split(Fs)
 Fn  = 0.5*(dot((Fsf), n) + abs(dot((Fsf), n)))
+
+
+
+
+# Flux limiting for q.
+alpha = Function(DG1)
+q_bar = Function(DG1)
+q_hat_bar = Function(DG1)
+
+# q+- factor in alpha
+
+q_plus = Function(DG1)
+w = TestFunction(DG1)
+q_plus_num = Function(DG1)
+q_plus_form = both(Fn * w)  * dS + Fn * w * q *ds
+assemble(q_plus_form, tensor=q_plus_num)
+q_plus.assign((1/c_plus) * q_plus_num)
+
+q_minus = Function(DG1)
+q_minus_num = Function(DG1)
+q_minus_form =- both(Fn * w)  * dS - Fn * w * q_in  *ds
+assemble(q_minus_form, tensor=q_minus_num)
+q_minus.assign((1/c_minus) * q_minus_num)
+
+#maximum bound for q
+qmax = Constant(2.0)
+qmin = Constant(1.0)
+#set alpha
+#alpha_expr = Min(1, ((1 + c_minus - c_plus)* qmax - q_hat_bar * (1 - c_plus) - c_minus * q_minus) / (c_plus * (q_hat_bar - q_plus)))
+alpha_expr = 0
+alpha_min_expr = Constant(1.0)
+#alpha_min_expr = Min(1, (q_hat_bar * (1 - c_plus) + c_minus * q_minus - (1 + c_minus + c_plus)* qmin) / (c_plus * (q_plus - q_hat_bar)))
+
+
+
+#variational problem for q
+L_q = phi * rho * q * dx + dtc*(q*dot(grad(phi),Fsf)*dx
+          - conditional(dot(Fsf, n) < 0, phi*dot(Fsf, n)*q_in, 0.0)*ds
+          - conditional(dot(Fsf, n) > 0, phi*dot(Fsf, n)*q, 0.0)*ds
+          - (phi('+') - phi('-'))*(Fn('+')*q('+') - Fn('-')*q('-'))*dS)
+qnew = Function(V)
+
+
+
+
+# set solvers for rho and q.
+params = {'ksp_type': 'preonly', 'pc_type': 'bjacobi', 'sub_pc_type': 'ilu'}
+prob_rho = LinearVariationalProblem(a, L_rho, drho)
+solv_rho = LinearVariationalSolver(prob_rho, solver_parameters=params)
+
+prob_q = LinearVariationalProblem(b, L_q, qnew)
+solv_q = LinearVariationalSolver(prob_q, solver_parameters=params)
+
+
+
+# begin looping
+t = 0.0
+step = 0
+output_freq = 2
+# stage
+i = 0
+
+if step % output_freq == 0:
+    rhos.append(rho.copy(deepcopy=True))
+    qs.append(q.copy(deepcopy=True))
+    print("t=", t)
+
+#Apply the limiter to q and density first and find beta, alpha.
+rho_bar.project(rho)
+limiter_rho.apply(rho)
+rho_hat_bar.project(rho)
+#Here rho is the value after applying the Kuzmin limiter i.e. rho_hat
+beta.assign(beta_expr)
+#apply the limiting scheme to density
+#rho.project(rho_hat_bar + beta * (rho - rho_hat_bar))
+print(f"stage{i},rho_max=", rho.dat.data.max())
+print(f"stage{i},rho_min=", rho.dat.data.min())
+
+q_bar.project(q)
+limiter_q.apply(q)
+q_hat_bar.project(q)
+alpha.assign(Min(alpha_expr,alpha_min_expr))
+q.project(q_hat_bar + alpha * (q - q_hat_bar))
+print(f"stage{i},q_max=", q.dat.data.max())
+print(f"stage{i},q_min=", q.dat.data.min())
+
+
+
+# Main Body
+while t < T - 0.5*dt:
+    #solve the density and the bounded advection
+    #first stage
+    #For Flux_1, it should be solved before rho is solved depending on the way it's defined.
+    #Fssolver1.solve()
+    #solv1_rho.solve()
+    #rho_new.assign(rho + drho)
+    #rho.assign(rho_new)
+    #rho limiting scheme, beta1 found.
+    rho_bar.project(rho)
+    limiter_rho.apply(rho)
+    rho_hat_bar.project(rho)
+    beta.assign(beta_expr)
+    #apply the limiting scheme
+    rho.project(rho_hat_bar + beta * (rho - rho_hat_bar))
+    #For rho_1
+    solv_rho.solve()
+    rho_new.assign(rho + drho)
+
+    #For q_1
+    solv_q.solve()
+    q.assign(qnew)
+
+
+    #q limiting scheme
+    q_bar.project(q)
+    limiter_q.apply(q)
+    q_hat_bar.project(q)
+    alpha.assign(Min(alpha_expr,alpha_min_expr))
+    q.project(q_hat_bar + alpha * (q - q_hat_bar))
+
+
+    #rho.assign(rho_new)
+    rho.assign(rho + drho)
+
+    print(f'stage{i,}rho_max=', rho.dat.data.max())
+    print(f'stage{i},rho_min=', rho.dat.data.min())
+    #print(f'stage{i},q_max=', q.dat.data.max())
+    #print(f'stage{i},q_min=', q.dat.data.min())
+
+    #$rho_data.write(rho)
+    #q_data.write(q)
+
+    #update the step and proceed to the next time step.
+    i+=1
+    step += 1
+    t += dt
+
+    if step % output_freq == 0:
+        rhos.append(rho.copy(deepcopy=True))
+        qs.append(q.copy(deepcopy=True))
+        print("t=", t)
